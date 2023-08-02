715b96849ed94f8ee754e71105fb26af787bbe68
Creator125 <obedagualimpia1@gmail.com>
2023-08-01 21:17:55 -0500

    Teminando Practica

---
+++ Variables/tipo_datos.js
@@ -0,0 +1,17 @@
+let myname = "Obed";
+let number = 10;
+let boolean = true;
+let array = [1, 2, 3, 4, 5];
+let object = {
+    myname: "Obed",
+    number: 10,
+    isStudent: true,
+    hoobies: ["Dibujar", "Progrmar", "Musica"],
+    address: {
+        street: "Av El Perrote",
+        number: 123,
+        city: "Medellin"
+    }
+};
+
+console.log(object);
