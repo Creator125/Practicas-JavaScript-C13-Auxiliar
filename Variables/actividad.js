dd7e2e9751270d02c3f7cc83c9ffc993a8b83a7e
Creator125 <obedagualimpia1@gmail.com>
2023-08-01 19:41:18 -0500

    Nueva practica

---
+++ Variables/actividad.js
@@ -0,0 +1,6 @@
+let myname = "Obed";
+let lastname = "Córdoba";
+let year = 22;
+let city = "Medellín";
+
+console.log(`Hola, mi nombre es ${myname} ${lastname}, vivo en ${city} y tengo ${year} años`);
---
+++ Variables/variables.js
@@ -0,0 +1,18 @@
+/*
+let a = 10;
+var b = 20;
+const c = 30;
+
+a = 200
+
+console.log({ a });
+console.log({ b });
+console.log({ c });
+
+console.table({a, b, c})
+*/
+
+let a  = 'Hola ',
+ b = 'Creator125'
+
+console.log(a + b);
