f823d03d2e07b082cc4c82d35ffa6350010af66a
Creator125 <obedagualimpia1@gmail.com>
2023-08-08 21:15:47 -0500

    Nueva practicas

---
+++ Clase 9 - depuracion y breakpoinst/depuracion_breakpoinst.js
@@ -0,0 +1,29 @@
+// console.log(miVariable);
+console.log(a)
+
+let a = 40,
+    b = 27,
+    c = "La vida",
+    d = "El perrote",
+    x = a + b;
+
+const saludo = c + d;
+
+c  = "Vino el perrote"
+
+console.log({a});
+console.table({a, b, saludo});
+
+var miVariable = "Esta es una varible"
+
+console.log(miVariable);
+
+// console.log(c)
+
+// function suma(a, b){
+//     return a + b;
+// }
+
+// resulado = suma(a, b);
+
+// console.log(resulado);
