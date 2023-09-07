4cc507857033555cda7c6bd8655f969fba123bb5
Creator125 <obedagualimpia1@gmail.com>
2023-09-06 20:03:08 -0500

    Agregando ejercicios

--- practica_ciclos_bucles/Ejercicio8-Hijos.js
+++ practica_ciclos_bucles/Ejercicio8-Hijos.js
@@ -4,3 +4,23 @@ tienen hijos y cuántos no tienen, además se desea conocer el promedio del núm
 de hijos
 */
 
+let n = parseInt(prompt("Ingrese la cantidad de usuarios: "));
+let i = 0
+let cant;
+let contHijos;
+let contNoHijos;
+
+while (i >= n) {
+    cant = parseInt(prompt("Ingrese la cantidad de hjos: "));
+
+    if(cant != 0){
+        contHijos++;
+    }else{
+        contNoHijos++;
+    }
+
+    i++;
+}
+
+let promHijos = contHijos / n;
+
